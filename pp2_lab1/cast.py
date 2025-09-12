52958f4510844fec8ee9be49342f14670de190be
SanRauangit <klassrumrauan@gmail.com>
2025-09-12 12:50:24 +0500

    initial commit

--- pp2_lab1/cast.py
+++ pp2_lab1/cast.py
@@ -7,6 +7,6 @@
 # z = float("3")   # z will be 3.0
 # w = float("4.2") # w will be 4.2
 
-# x = str("s1") # x will be 's1'
-# y = str(2)    # y will be '2'
-# z = str(3.0)  # z will be '3.0'
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
